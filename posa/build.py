b0296e8f0f81075cfc68cdfcfaf5f2c6c7a10575
Greg Wilson <gvwilson@third-bit.com>
2017-02-11 08:22:21 -0500

    Updating print statement

--- posa/build.py
+++ posa/build.py
@@ -104,8 +104,8 @@ def main(chapters=[], epub=False, pdf=False, html=False, mobi=False, pandoc_epub
         build_epub(process_chapters, pandoc_epub)
 
     if mobi and not epub:
-        print "Can't build .mobi; depends on .epub."
-        print "Use --epub --mobi to build .mobi file."
+        print("Can't build .mobi; depends on .epub.")
+        print("Use --epub --mobi to build .mobi file.")
     elif mobi:
         build_mobi()
 
@@ -147,10 +147,10 @@ def build_epub(chapter_markdowns, pandoc_epub):
     cmd = "{pandoc} --chapters -S -f markdown+mmd_title_block --highlight-style=kate -o posa.epub epubtitle.txt introduction.markdown {markdowns}"
     if pandoc_epub:
         run(cmd.format(pandoc=pandoc_path, markdowns=" ".join(basenames)))
-        print cmd.format(pandoc=pandoc_path, markdowns=" ".join(basenames))
+        print(cmd.format(pandoc=pandoc_path, markdowns=" ".join(basenames)))
     import subprocess as sp
     output = " ".join(open('image-list.txt').read().splitlines())
-    print 'zip posa.epub META-INF mimetype nav.xhtml toc.ncx stylesheet.css content.opf ' + output
+    print('zip posa.epub META-INF mimetype nav.xhtml toc.ncx stylesheet.css content.opf ' + output)
     sp.check_output(
         'zip posa.epub META-INF mimetype nav.xhtml toc.ncx stylesheet.css content.opf ' + output,
         shell=True)
@@ -204,7 +204,7 @@ def preprocessor_command(chapter_markdown):
     temp = 'python preprocessor.py --output=tex/{basename}.markdown --markdown {md}'
     basename = getbasename(chapter_markdown)
     result = temp.format(basename=basename, md=chapter_markdown)
-    print result
+    print(result)
     return (result, basename)
 
 def postprocessor_command(basename):
@@ -216,22 +216,22 @@ def pandoc_cmd(chapter_markdown):
     result = envoy.run(cmd)
     new_chapter_markdown = basename + '.markdown'
     if result.status_code != 0:
-        print result.std_err
+        print(result.std_err)
     else:
-        print result.std_out
+        print(result.std_out)
     result = envoy.run(_pandoc_cmd(new_chapter_markdown))
     if result.status_code != 0:
-        print result.std_err
+        print(result.std_err)
     else:
-        print result.std_out
+        print(result.std_out)
     result2 = envoy.run(postprocessor_command(basename))
     return result2
 
 def run(cmd):
-    print cmd
+    print(cmd)
     result = envoy.run(cmd)
-    print result.std_out
-    print result.std_err
+    print(result.std_out)
+    print(result.std_err)
     return result
 
 if __name__ == '__main__':
